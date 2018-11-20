b4006f7f1b64a7f756652dfd62c0168611a56c41
mklinovsky <michal.klinovsky@gmail.com>
2018-11-20 17:15:29 +0100

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomerFilterContainerComponent } from './components/customer-filter-container/customer-filter-container.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerFilterContainerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/customer-filter-container/customer-filter-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-filter-container',
+  templateUrl: './customer-filter-container.component.html',
+  styleUrls: ['./customer-filter-container.component.scss']
+})
+export class CustomerFilterContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
